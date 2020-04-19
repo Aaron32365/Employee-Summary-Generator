2b0e3a5e7357380148651aa58c6f3d3554342515
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-04-19 14:49:35 -0700

    added library to include classes; Employee class almost finished. added tests - complete

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,21 @@
+class Employee {
+    constructor(name, id, email,){
+        this.name = name
+    }
+
+    getName(){
+        return this.name
+    }
+    
+    getId(){
+        return this.id
+    }
+
+    getEmail(){
+        return this.email
+    }
+
+    //add getRole function
+}
+
+module.exports = Employee
